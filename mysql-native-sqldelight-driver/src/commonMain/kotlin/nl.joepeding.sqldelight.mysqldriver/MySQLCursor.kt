4a1acf597647540c960811c3bd53476392089569
Joep Eding <joep.eding@nedap.com>
2023-09-03 06:58:32 +0200

    fix: correct, reorder and simply buffer assignment

--- mysql-native-sqldelight-driver/src/commonMain/kotlin/nl.joepeding.sqldelight.mysqldriver/MySQLCursor.kt
+++ mysql-native-sqldelight-driver/src/commonMain/kotlin/nl.joepeding.sqldelight.mysqldriver/MySQLCursor.kt
@@ -44,25 +44,25 @@ class MySQLCursor(
                 MYSQL_TYPE_SHORT,
                 MYSQL_TYPE_LONG,
                 MYSQL_TYPE_INT24,
+                MYSQL_TYPE_BIT,
                 MYSQL_TYPE_LONGLONG,
-                MYSQL_TYPE_TIMESTAMP,
-                MYSQL_TYPE_TIMESTAMP2 -> memScope.alloc<MYSQL_TIME>()
+                MYSQL_TYPE_YEAR -> memScope.alloc<LongVar>()
                 MYSQL_TYPE_DECIMAL,
                 MYSQL_TYPE_NEWDECIMAL,
                 MYSQL_TYPE_FLOAT,
                 MYSQL_TYPE_DOUBLE -> memScope.alloc<DoubleVar>()
-                MYSQL_TYPE_BIT -> memScope.alloc<LongVar>()
-                MYSQL_TYPE_DATE -> memScope.alloc<MYSQL_TIME>()
+                MYSQL_TYPE_DATE,
                 MYSQL_TYPE_TIME,
-                MYSQL_TYPE_TIME2 -> memScope.alloc<MYSQL_TIME>()
+                MYSQL_TYPE_TIME2,
                 MYSQL_TYPE_DATETIME,
-                MYSQL_TYPE_DATETIME2 -> memScope.alloc<MYSQL_TIME>()
-                MYSQL_TYPE_YEAR -> memScope.alloc<LongVar>()
+                MYSQL_TYPE_DATETIME2,
+                MYSQL_TYPE_TIMESTAMP,
+                MYSQL_TYPE_TIMESTAMP2 -> memScope.alloc<MYSQL_TIME>()
                 MYSQL_TYPE_STRING,
                 MYSQL_TYPE_VAR_STRING,
                 MYSQL_TYPE_BLOB,
                 MYSQL_TYPE_SET,
-                MYSQL_TYPE_ENUM-> memScope.allocArray<ByteVar>(field.max_length.toInt()).pointed
+                MYSQL_TYPE_ENUM -> memScope.allocArray<ByteVar>(field.max_length.toInt()).pointed
                 MYSQL_TYPE_GEOMETRY -> TODO()
                 MYSQL_TYPE_NULL -> TODO()
                 else -> { error("Encountered unknown field type: ${field.type}") }
