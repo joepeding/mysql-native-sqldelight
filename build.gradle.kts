7609f2b1c38bfe8e40b333885d96dca4b78d46f6
Joep Eding <joep.eding@nedap.com>
2023-12-03 10:50:57 +0100

    ci: implement publishing pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
-group = "nl.joepeding.sqldelight"
-version = rootProject.file("version.properties").readText().trim()
-
 plugins {
     alias(libs.plugins.sqldelight) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.grammarKitComposer) apply false
     alias(libs.plugins.dokka) apply true
+}
+
+allprojects {
+    group = "nl.joepeding.sqldelight"
+    version = rootProject.file("version.properties").readText().trim()
 }
 
 allprojects {
